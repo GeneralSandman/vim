6863027e48ef5d2f87698b09edfc03a66f59df62
GeneralSandman <13287069774@163.com>
2018-10-21 11:05:41 +0800

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bash/sh
 
+# install zsh
 sudo apt-get install zsh curl
-
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install vim-plug
+mkdir ~/.vim/autoload/
+cd ~/.vim/autoload/
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 sudo apt-get install ctags
 
